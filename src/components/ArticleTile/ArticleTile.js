7db8833b6396c647725050086f205d1cbfc34df0
nandini <nininandini@gmail.com>
2020-10-19 16:22:27 -0700

    Add pagination.

--- src/components/ArticleTile/ArticleTile.js
+++ src/components/ArticleTile/ArticleTile.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import ImageTile from '../../components/ImageTile/ImageTile';
+import React, { Component } from "react";
+import ImageTile from "../../components/ImageTile/ImageTile";
 
-    class ArticleTile extends Component{
-       
-        render(){
-           let Imagetile=null;
-            if (this.props.media!==null){
-                Imagetile=this.props.media.map(Image=>{
-                  //  console.log("media-image",Image['media-metadata']) 
-                  return  (
-                   
-                     <ImageTile 
-                             key={Image.copyright}
-                             mediametadata={Image['media-metadata']} ></ImageTile>
-                  )
-              });
-            }
+class ArticleTile extends Component {
+  render() {
+    let Imagetile = null;
+    if (this.props.media !== null) {
+      Imagetile = this.props.media.map((Image) => {
+        //  console.log("media-image",Image['media-metadata'])
+        return (
+          <ImageTile
+            key={Image.copyright}
+            mediametadata={Image["media-metadata"]}
+          ></ImageTile>
+        );
+      });
+    }
 
-            return(
-                <li className="article-tile">
-                    <div className="contentinside">
-                     {Imagetile}
-                    <a className="anchorstyle" href={this.props.url} target="_blank">{this.props.title}</a>
-                    <p>{this.props.publisheddate}</p>
-                    </div>
-
-                </li>
-              )
-           }
-
+    return (
+      <li className="article-tile">
+        <div className="contentinside">
+          {Imagetile}
+          <a className="anchorstyle" href={this.props.url} target="_blank">
+            {this.props.title}
+          </a>
+          <p>{this.props.publisheddate}</p>
+        </div>
+      </li>
+    );
+  }
 }
 
-export default ArticleTile
+export default ArticleTile;
--- src/containers/Articles/Articles.js
+++ src/containers/Articles/Articles.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 import ArticleTile from '../../components/ArticleTile/ArticleTile';
 import './Articles.css';
 
 class Articles extends Component{
  
-    state={
-        articledata:[]
-    }
+    constructor(props) {
+      super(props);
+      this.state = {
+        offset: 0,
+        articledata:[],
+        perPage: 10,
+        currentPage: 0
+      };
+      this.handlePageClick = this
+          .handlePageClick
+          .bind(this);
+  }
+
+    receivedData() {
+      console.log(this.props);
+      axios.get("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=rOdFTBy8ifr0y2EhhG72MUmeGGADIl5s")
+      .then(response => {
+           const data = response.data.results;
+           this.setState({articledata:data})
+        //        this.setState({pageCount: Math.ceil(data.length / this.state.perPage),           
+        //     articles
+        // })
+      }).catch(error => {
+       console.log(error); 
+      // this.setState({error:true})  ; 
+      });      
+  }
+
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState({
+        currentPage: selectedPage,
+        offset: offset
+    }, () => {
+        this.receivedData()
+    });
+
+  }
 
     componentDidMount(){
-       console.log(this.props);
-        axios.get("https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=rOdFTBy8ifr0y2EhhG72MUmeGGADIl5s")
-        .then(response => {
-          console.log("response", response);
-             const data = response.data.results;
-                console.log("data",data);
-
-             this.setState({articledata:data})
-        }).catch(error => {
-         console.log(error); 
-        // this.setState({error:true})  ; 
-        });      
-    }
+      this.receivedData();       
+    };
 
     postSelectedHandler=(id)=>{
       //  this.setState({selectedPostId:id});
@@ -36,11 +63,9 @@ class Articles extends Component{
     render(){
         let articles= <p style={{textAlign:"center"}}>Something went wrong</p>
         if (! this.state.error){
-          articles=this.state.articledata.map(article=>{
-            console.log("article.title",article.title);
-            console.log("article",article);
+          const data = this.state.articledata;
+          articles=data.map(article=>{
               return  (
-              //  <Link to={'/'+ article.id}   key={article.id} > 
                  <ArticleTile 
                          key={article.id}
                          title={article.title} 
@@ -49,9 +74,10 @@ class Articles extends Component{
                          publisheddate={article.published_date}
                          media={article.media}
                          clicked={()=>{this.postSelectedHandler(article.id)}}/>
-              // </Link>
               )
           });
+       
+      
         }
         return(
           <div className="divarticles">
@@ -61,6 +87,18 @@ class Articles extends Component{
             </ul>
           </section>
           {/* <Route path={this.props.match.url + '/:id' } exact component={FullPost}/>   */}
+          <ReactPaginate
+                  previousLabel={"prev"}
+                  nextLabel={"next"}
+                  breakLabel={"..."}
+                  breakClassName={"break-me"}
+                  pageCount={this.state.pageCount}
+                  marginPagesDisplayed={2}
+                  pageRangeDisplayed={5}
+                  onPageChange={this.handlePageClick}
+                  containerClassName={"pagination"}
+                  subContainerClassName={"pages pagination"}
+                  activeClassName={"active"}/>
           </div>
 
         )
